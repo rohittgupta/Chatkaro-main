a3a5dc71e4ad0ffc6bd159e5a7b023fcfc186801
rohittgupta <rohitgupta222004@gmail.com>
2025-12-27 16:04:56 +0530

    commit18

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,12 +1,10 @@
 // axios => used for call of https requests..
-
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL:"http://localhost:5002/api",
+    baseURL: import.meta.env.VITE_API_URL + "/api" || "http://localhost:5002/api",
     withCredentials: true,
 });
-
 
 
 
